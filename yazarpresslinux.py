ee2c33e40cf6bd0818020b2cc7541513554311f8
Volkan Özdemir Yazılım Hizmetleri <integrumart@gmail.com>
2026-01-12 10:11:01 +0300

    update

--- yazarpresslinux.py
+++ yazarpresslinux.py
@@ -4,7 +4,6 @@ import sys
 def main():
     print("YazarPressLinux - İntegrum Mimari Gururla Sunar!")
     print("WordPress Yönetim Aracı Başlatılıyor...")
-    # Buraya ileride WordPress API bağlantı kodlarını döşeyeceğiz.
 
 if __name__ == "__main__":
     main()
